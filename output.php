ed2ce56b07411a3fdc8cffa7c3ba42c4def022ef
Mark Kimsal <metrofindings@gmail.com>
2014-11-26 19:18:21 -0500

    Allow ?format=json parameter to trigger JSON output.

--- output.php
+++ output.php
@@ -37,16 +37,17 @@ class Metrofw_Output {
 		}
 
 		if ($request->isAjax) {
+			_clearHandlers('output');
 			//sometimes ajax wants HTML, sometimes it doesn't
-			if (strpos($_SERVER['HTTP_ACCEPT'], 'html')===FALSE) {
+			if (strpos($_SERVER['HTTP_ACCEPT'], 'html')===FALSE
+			    || $request->cleanString('format') == 'json') {
+
 				header('Content-type: application/json');
 				echo json_encode($response->sectionList);
 				//stop HTML output
-				_iCanOwn('output', 'metrofw/output.php::noop');
 			}
 			//enable partial HTML rendering
 			//TODO: remove hardcoded dependency?
-			_iCanOwn('output', 'metrofw/output.php::noop');
 			Metrofw_Template::parseSection('main');
 		}
 	}
