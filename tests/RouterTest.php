0e3554b84f79067f12d1ab34378f7baa70450657
Mark Kimsal <metrofindings@gmail.com>
2014-11-26 19:41:01 -0500

    Add router test.

---
+++ tests/RouterTest.php
@@ -0,0 +1,77 @@
+<?php
+include_once(dirname(__DIR__).'/kernel.php');
+include_once(dirname(__DIR__).'/router.php');
+include_once(dirname(__DIR__).'/request.php');
+
+class Metrofw_Tests_Router extends PHPUnit_Framework_TestCase { 
+
+	public $kernel = NULL;
+
+	public function setUp() {
+		$this->router    = new Metrofw_Router();
+		$this->request   = new Metrofw_Request();
+	}
+
+	public function test_default_routing_uses_main_as_modName() {
+		$this->request->requestedUrl = '/app/func/';
+		$this->router->autoRoute($this->request);
+		$this->assertEquals(
+			'app',
+			$this->request->appName 
+		);
+		$this->assertEquals(
+			'main',
+			$this->request->modName 
+		);
+		$this->assertEquals(
+			'func',
+			$this->request->actName 
+		);
+	}
+
+	public function test_route_rules() {
+
+		$this->container = new Metrodi_Container();
+		$this->kernel    = new Metrofw_Kernel($this->container);
+
+
+		$this->container->set('route_rules', array());
+
+		$this->container->set('route_rules', 
+			array_merge(array('/:appName'=>array( 'modName'=>'main', 'actName'=>'main' )),
+			_get('route_rules')));
+
+		$this->container->set('route_rules', 
+			array_merge(array('/:appName/:modName'=>array( 'actName'=>'main' )),
+			_get('route_rules')));
+
+		$this->container->set('route_rules', 
+			array_merge(array('/:appName/:modName/:actName'=>array(  )),
+			_get('route_rules')));
+
+		$this->container->set('route_rules', 
+			array_merge(array('/:appName/:modName/:actName/:arg'=>array(  )),
+			_get('route_rules')));
+
+
+		$this->request->requestedUrl = '/app/mod/func/';
+		$this->router->autoRoute($this->request, $this->kernel, $this->container);
+
+		$this->assertEquals(
+			'app',
+			$this->request->appName 
+		);
+		$this->assertEquals(
+			'mod',
+			$this->request->modName 
+		);
+		$this->assertEquals(
+			'func',
+			$this->request->actName 
+		);
+
+		$this->assertTrue(
+			isset($this->kernel->serviceList['process']) 
+		);
+	}
+}
