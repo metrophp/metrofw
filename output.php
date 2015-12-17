f5696e858a9afcb79d83247bcec0104f8876e904
Mark Kimsal <metrofindings@gmail.com>
2015-12-17 10:42:07 -0500

    Add 403 Forbidden status

--- output.php
+++ output.php
@@ -93,6 +93,10 @@ class Metrofw_Output {
 			header('HTTP/1.1 401 Unauthorized');
 			break;
 
+			case 403:
+			header('HTTP/1.1 403 Forbidden');
+			break;
+
 			case 404:
 			header('HTTP/1.1 404 File Not Found');
 			break;
